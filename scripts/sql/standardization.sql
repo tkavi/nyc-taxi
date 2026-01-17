d025b3cdc1bcf1560b07dd5b4c30b1e475f77f05
tkavi <tkavita.kavs@gmail.com>
2026-01-17 15:31:11 -0600

    standardizing

--- scripts/sql/standardization.sql
+++ scripts/sql/standardization.sql
@@ -4,31 +4,28 @@ SELECT
     tpep_pickup_datetime,
     tpep_dropoff_datetime,
     CAST(COALESCE(passenger_count,0) AS INT) AS passenger_count,
-    COALESCE(trip_distance,0),
+    COALESCE(trip_distance,0) AS trip_distance,
     -- RatecodeID NULL to 99 as per dictionary 
-    CASE 
-        WHEN RatecodeID IS NULL THEN 99 
-        ELSE CAST(RatecodeID AS INT) 
-    END AS RatecodeID,
-    -- Other value to NULL 
+    CAST(COALESCE(RatecodeID,99) AS INT) AS RatecodeID,
+    -- Other value to 'U'(Unknown) 
     CASE 
         WHEN TRIM(UPPER(store_and_fwd_flag)) = 'Y' THEN 'Y' 
         WHEN TRIM(UPPER(store_and_fwd_flag)) = 'N' THEN 'N' 
-        ELSE NULL 
+        ELSE 'U' 
     END AS store_and_fwd_flag,
     PULocationID,
     DOLocationID,
     CAST(payment_type AS INT) AS payment_type,
-    COALESCE(fare_amount,0)
-    COALESCE(extra,0),
-    COALESCE(mta_tax,0),
-    COALESCE(tip_amount,0),
-    COALESCE(tolls_amount,0),
-    COALESCE(improvement_surcharge,0),
-    COALESCE(total_amount,0),
-    COALESCE(congestion_surcharge,0),
-    COALESCE(airport_fee,0),
-    COALESCE(cbd_congestion_fee,0)
+    COALESCE(fare_amount,0) AS fare_amount,
+    COALESCE(extra,0) AS extra,
+    COALESCE(mta_tax,0) AS mta_tax,
+    COALESCE(tip_amount,0) AS tip_amount,
+    COALESCE(tolls_amount,0) AS tolls_amount,
+    COALESCE(improvement_surcharge,0) AS improvement_surcharge,
+    COALESCE(total_amount,0) AS total_amount,
+    COALESCE(congestion_surcharge,0) AS congestion_surcharge,
+    COALESCE(airport_fee,0) AS airport_fee,
+    COALESCE(cbd_congestion_fee,0) AS cbd_congestion_fee
 FROM raw_nyc_taxi_data
 WHERE 
     -- Valid TPEP providers
